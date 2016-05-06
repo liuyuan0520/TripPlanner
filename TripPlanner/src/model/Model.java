dd9272c663abb941d95730164690339990363838
ryancmu <zji1@andrew.cmu.edu>
2016-05-06 19:51:06 -0400

    HAHAHAHAHAHAHAHAHAHA

---
+++ TripPlanner/src/model/Model.java
@@ -0,0 +1,29 @@
+package model;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import org.genericdao.ConnectionPool;
+import org.genericdao.DAOException;
+
+public class Model {
+
+    // private StopDAO stopDAO;
+
+    public Model(ServletConfig config) throws ServletException {
+        try {
+            String jdbcDriver = config.getInitParameter("jdbcDriverName");
+            String jdbcURL = config.getInitParameter("jdbcURL");
+
+            ConnectionPool pool = new ConnectionPool(jdbcDriver, jdbcURL);
+            // stopDAO = new StopDAO("Stop", pool);
+        } catch (DAOException e) {
+            throw new ServletException(e);
+        }
+    }
+
+    // public StopDAO getStopDAO() {
+    // return stopDAO;
+    // }
+
+}
