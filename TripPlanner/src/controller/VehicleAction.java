c16dda3560659aa39f864cdffbbdc21d7815bd22
ryancmu <zji1@andrew.cmu.edu>
2016-05-08 01:04:37 -0400

    Track vehicle by vid, ajax

---
+++ TripPlanner/src/controller/VehicleAction.java
@@ -0,0 +1,36 @@
+/**
+ * Get Vehicle location by AJAX
+ * @author Zhihao Ji
+ * @date 05/07/2016
+ */
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.simple.JSONObject;
+
+import util.Common;
+
+public class VehicleAction extends Action {
+
+    @Override
+    public String getName() {
+        return "vehicle.do";
+    }
+
+    @Override
+    public String perform(HttpServletRequest request) {
+
+        String vid = request.getParameter("vid");
+        JSONObject vehicleInfo = (JSONObject) Common.getVehicle(vid).get(0);
+        double lon = (double) vehicleInfo.get("lon");
+        double lat = (double) vehicleInfo.get("lat");
+        JSONObject location = new JSONObject();
+        location.put("lat", lat);
+        location.put("lon", lon);
+
+        request.setAttribute("realLocation", location);
+        return "vehicle.ajax";
+    }
+
+}
