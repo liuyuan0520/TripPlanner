52be05cb75dc0eded2778ad04ddd73f1e58a742f
ryancmu <zji1@andrew.cmu.edu>
2016-05-09 15:01:24 -0400

    finish the ajax test

--- TripPlanner/src/controller/TestAction.java
+++ TripPlanner/src/controller/TestAction.java
@@ -22,9 +22,12 @@ public class TestAction extends Action {
     public String perform(HttpServletRequest request) {
 
         String vid = request.getParameter("vid");
-        JSONObject vehicleInfo = (JSONObject) Common.getVehicle(vid).get(0);
-        double lon = (double) vehicleInfo.get("lon");
-        double lat = (double) vehicleInfo.get("lat");
+        System.out.println("vid: " + vid);
+        JSONObject vehicleInfo = (JSONObject) Common.getVehicle("3225").get(0);
+        double lon = Double.parseDouble(((String) vehicleInfo.get("lon"))
+                .trim());
+        double lat = Double.parseDouble(((String) vehicleInfo.get("lat"))
+                .trim());
         JSONObject location = new JSONObject();
         location.put("lat", lat);
         location.put("lon", lon);
